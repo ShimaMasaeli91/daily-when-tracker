a90447080d72a5662d1373afb48cfea33bee7943
ShimaMasaeli91 <shima.physics@gmail.com>
2023-11-05 16:33:23 +0330

    clean code.

--- src/components/dailyWhenTracker.jsx
+++ src/components/dailyWhenTracker.jsx
@@ -21,29 +21,11 @@ const DailyWhenTracker = () => {
     energeticScore: 0,
   });
 
-  debugger;
-  console.info(answers);
-  console.info("Info ------");
-  console.log("log ------");
-  console.warn("warn ------");
-  console.error("error ------");
-
-  //answers is a state variable that contains the current values of the user's answers to the three questions. It is initialized to an empty object, and it is updated whenever the user changes an answer.
-  //setAnswers is a function that can be used to update the answers state variable. It takes a new object as an argument, and it overwrites the current answers state variable with the new object.
   const handleChange = (e) => {
     const { name, value } = e.target;
     setAnswers((prevAnswers) => ({ ...prevAnswers, [name]: value }));
   };
 
-  //The handleSubmit function is an asynchronous function that is called when the user submits the form.
-  //It does the following:
-  // It prevents the default form submission behavior.
-  // It creates a new fetch request to the /api/track-daily-when endpoint.
-  // It sets the method of the request to POST.
-  // It sets the Content-Type header of the request to application/json.
-  // It sets the body of the request to the JSON-serialized answers object.
-  // It awaits the response from the server.
-  // It does something with the response, such as checking for errors or displaying a success message.
   const handleSubmit = async (e) => {
     e.preventDefault();
 
